f0bcc21220e22e873bc6a21d6ee50135c311a3c1
zhanglei <lei.zhang@sunscell.com>
2017-01-09 10:37:05 +0800

    merge

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
     module: {
         loaders: [{
                 test: /\.ts$/,
-                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+                loaders: ['ts-loader', 'angular2-template-loader']
             },
             {
                 test: /\.html$/,
