6abc8f025121469970fcf69f5e1f394a50e448ec
zhanglei <lei.zhang@sunscell.com>
2017-03-01 11:21:01 +0800

    merge

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/chart.js/index.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/moment/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/tween.js/index.d.ts" />
-/// <reference path="globals/underscore/index.d.ts" />
